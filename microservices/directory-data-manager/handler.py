8c5075301228f7a3e84a77f422d6c8ccf99e8235
Gregg Cowell <gdcowell@googlemail.com>
2022-09-08 17:14:02 +0100

    Reintroduce matrix step

--- microservices/directory-data-manager/handler.py
+++ microservices/directory-data-manager/handler.py
@@ -7,7 +7,7 @@ def index(event, context):
         "input": event,
     }
 
-    response1 = "Lets add some ammendmentioz"
+    response1 = "Lets add some ammendmentioo"
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
