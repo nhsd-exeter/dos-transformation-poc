a2749072470aabaaa5be6ffec0ad9f421a8afad2
Gregg Cowell <gdcowell@googlemail.com>
2022-09-08 15:51:47 +0100

    Test pipeline function

--- microservices/directory-data-manager/handler.py
+++ microservices/directory-data-manager/handler.py
@@ -7,7 +7,7 @@ def index(event, context):
         "input": event,
     }
 
-    response1 = {"statusCode": 200, "body": json.dumps(body)}
+    response1 = "Lets add a change"
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
