e25417e2a4e91228b4143b66c581863cf2900ac7
Gregg Cowell <gdcowell@googlemail.com>
2022-09-15 15:38:10 +0100

    Add env variables

--- microservices/directory-data-manager/app.py
+++ microservices/directory-data-manager/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World! I am the Directory-Data-Manager'
 
-    #change me
+    #change me 
 
 if __name__ == '__main__':
     app.run(debug=True)
