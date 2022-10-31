df0b6c795ea45cc3e14f1e73ace6814854abca00
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-10-31 18:51:49 +0000

    Add step function orchestration

--- microservices/directory-search/app.py
+++ microservices/directory-search/app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-#var = 'updated'
+# #var = 'updated'
 
-@app.route('/')
-def hello():
-    return 'Hello, World! I am the Directory-Search Service!'
+# @app.route('/')
+# def hello():
+#     return 'Hello, World! I am the Directory-Search Service!'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+def lambda_handler(event, context):
+
+    print(event)
+    print(context)
