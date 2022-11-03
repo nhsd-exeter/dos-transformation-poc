b36b524846bf97d2aa830e3455e2da7e6bb7b7a1
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-03 16:19:27 +0000

    Add functionality to search profile manager

--- microservices/search-profile-manager/app.py
+++ microservices/search-profile-manager/app.py
@@ -1,13 +1,37 @@
 from chalice import Chalice
-#deploy
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
 app = Chalice(app_name="helloworld")
+dynamodb = boto3.resource('dynamodb')
+id = app.current_request.query_params.get('id')
 
 @app.route("/searchprofiles", methods=['GET'])
-def index():
+def get_search_profile():
+
+    search_profiles_table = dynamodb.Table('search-profiles')      
+    
+    search_profile_resp = search_profiles_table.get_item(
+            Key={
+                'id' : search_profile_id,
+            }
+        )
+    
+    search_profile = search_profile_resp['Item']
+
+    return {"hello": search_profile_resp}
+
+
+@app.route("/searchprofiles", methods=['POST'])
+def create_search_profile():
     id = app.current_request.query_params.get('id')
     return {"hello": id}
 
-@app.route('/{id}', methods=['GET'])
-def get_search_profile(id):
-    return {'search-profile': id}
+
+@app.route("/searchprofiles", methods=['DELETE'])
+def delete_search_profile():
+    id = app.current_request.query_params.get('id')
+    return {"hello": id}
+
 
