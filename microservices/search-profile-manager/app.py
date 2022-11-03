aaf4db58786020fc8fde87a1fe9f9e628e98920a
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-03 10:55:23 +0000

    Update app.py

--- microservices/search-profile-manager/app.py
+++ microservices/search-profile-manager/app.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#var = 'updated'
-
 @app.route('/')
 def hello():
-    return 'Hello, World! I am the Search-Profile-Manager!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return 'Hello, World!'
+    
+@app.route('/say-my-name')
+def hello_world():
+    return 'Hello, ' + request.args.get('name')
