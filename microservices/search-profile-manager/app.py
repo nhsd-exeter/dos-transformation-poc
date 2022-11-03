2a9388602b09ed840944f1a277c584f0ea2c6f6f
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-03 15:01:33 +0000

    Reinstate Flask

--- microservices/search-profile-manager/app.py
+++ microservices/search-profile-manager/app.py
@@ -1,7 +1,12 @@
-from chalice import Chalice
-#deploy
-app = Chalice(app_name="helloworld")
+from flask import Flask
 
-@app.route("/")
-def index():
-    return {"hello": "world"}
+app = Flask(__name__)
+
+#var = 'updated'
+
+@app.route('/')
+def hello():
+    return 'Hello, World! I am the Directory-Data-Manager'
+
+if __name__ == '__main__':
+    app.run(debug=True)
