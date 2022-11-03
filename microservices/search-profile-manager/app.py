1eb879c3ecb181c1e16f7593a9f84523cdf57899
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-03 15:13:26 +0000

    Add routes to SPM

--- microservices/search-profile-manager/app.py
+++ microservices/search-profile-manager/app.py
@@ -6,3 +6,7 @@ app = Chalice(app_name="helloworld")
 def index():
     return {"hello": "world"}
 
+@app.route('/{id}', methods=['GET'])
+def get_search_profile(id):
+    return {'search-profile': id}
+
