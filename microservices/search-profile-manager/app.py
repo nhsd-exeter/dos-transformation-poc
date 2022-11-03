15390c3126316c71fe4aaa061c29b39a2056b4be
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-03 15:32:44 +0000

    Add route to lambda

--- microservices/search-profile-manager/app.py
+++ microservices/search-profile-manager/app.py
@@ -2,7 +2,7 @@ from chalice import Chalice
 #deploy
 app = Chalice(app_name="helloworld")
 
-@app.route("/", methods=['GET'])
+@app.route("/searchprofiles", methods=['GET'])
 def index():
     return {"hello": "world"}
 
