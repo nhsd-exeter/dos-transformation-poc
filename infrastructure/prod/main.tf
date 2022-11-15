eda310d24dde1f03f2439eb4c34737223a25d041
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-15 19:48:12 +0000

    Disable internal db

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -595,7 +595,7 @@ resource "aws_elasticsearch_domain" "directory_search" {
 
   advanced_security_options {
     enabled = true
-    internal_user_database_enabled = true
+    internal_user_database_enabled = false
     master_user_options {
       master_user_arn = "arn:aws:iam::202422821117:role/github"
     }
