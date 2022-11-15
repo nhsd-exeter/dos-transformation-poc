9744fffba584835d9c6f9b5a40fe9bec1a95feef
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-15 18:40:04 +0000

    Add python provisioner script

---
+++ infrastructure/prod/elastic/configure_elastic.py
@@ -0,0 +1,79 @@
+import boto3
+import sys
+import json
+import requests
+from requests_aws4auth import AWS4Auth
+
+region = sys.argv[1]
+service = 'es'
+credentials = boto3.Session().get_credentials()
+awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+headers = { "Content-Type": "application/json" }
+index_name = 'directory-index'
+user_path = "/_plugins/_security/api/internalusers/"
+
+
+def configure_elastic(host, search_arn, relay_arn):
+
+
+    if check_user_exists("search_user") != true:
+        search_user = {
+            "password": "kirkpass",
+            "opendistro_security_roles": ["readall"],
+            "backend_roles": [search_arn],
+        }
+
+        create_user("search_user", search_user, search_arn)
+
+    if check_user_exists("relay_user") != true:
+        relay_user = {
+            "password": "kirkpass",
+            "opendistro_security_roles": ["all_access"],
+            "backend_roles": [relay_arn],
+        }
+
+        create_user("relay_user", relay_user, relay_arn)
+    
+
+    if check_index_exists(index_name) != true:
+        create_index(index_name)
+
+
+
+def check_user_exists(host, name):
+    url = host + user_path + name
+    r = requests.get(url, auth=awsauth, headers=headers)
+    jsonResponse = r.json()
+
+    if jsonResponse["status"] == "NOT_FOUND":
+        return false
+    else:
+        return true
+
+
+
+def create_user(name, data, arn):
+    url = host + user_path + name
+    r = requests.put(url, auth=awsauth, headers=headers, data=json.dumps(data))
+
+
+
+def check_index_exists(index_name):
+    url = host + "/" + index_name
+    r = requests.get(url, auth=awsauth, headers=headers)
+    jsonResponse = r.json()
+
+    if jsonResponse["error"]["type"] == "index_not_found_exception":
+        return false
+    else:
+        return true
+
+
+def create_index(index_name):
+    url = host + "/" + index_name
+    r = requests.put(url, auth=awsauth, headers=headers)
+
+
+
+if __name__== "__main__":
+   configure_elastic(sys.argv[2], sys.argv[3], sys.argv[4])
--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -595,12 +595,17 @@ resource "aws_elasticsearch_domain" "directory_search" {
       ]
     }
   POLICY
+
   }
 
-  
-
-
-
+  resource "null_resource" "elastic_provisioner_script" {
+  provisioner "remote-exec" {
+      inline = [
+        "pip install -r requirements.txt --target .",
+        "python3 configure_elastic.py ${var.aws_region} ${aws_elasticsearch_domain.directory_search.endpoint} ${module.directory-search-lambda.lambda_function_arn} ${module.directory-data-relay-lambda.lambda_function_arn}",
+      ]
+    }
+  }
 
 
 ######
