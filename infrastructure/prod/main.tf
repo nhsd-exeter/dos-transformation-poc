3eae6f913889b688e957781d350666cfc636ba2c
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-15 19:27:45 +0000

    TLS Policy

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -590,6 +590,7 @@ resource "aws_elasticsearch_domain" "directory_search" {
 
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
   }
 
   advanced_security_options {
