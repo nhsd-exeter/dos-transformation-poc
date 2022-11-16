20767828aaf4d68296ab5bda08bcff4af10b9af7
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-16 10:14:56 +0000

    Fix provisioner script

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -628,16 +628,16 @@ resource "aws_elasticsearch_domain" "directory_search" {
 
 
   resource "null_resource" "elastic_provisioner_script" {
-  provisioner "local-exec" {
-      inline = [
-        "pip install -r requirements.txt --target .",
-        "python3 configure_elastic.py ${var.aws_region} ${aws_elasticsearch_domain.directory_search.endpoint} ${module.directory-search-lambda.lambda_function_arn} ${module.directory-data-relay-lambda.lambda_function_arn}",
-      ]
-
-      triggers = {
-        always_run = timestamp()
+    provisioner "local-exec" {
+      command = <<EOT
+        pip install -r requirements.txt --target .
+        python3 configure_elastic.py ${var.aws_region} ${aws_elasticsearch_domain.directory_search.endpoint} ${module.directory-search-lambda.lambda_function_arn} ${module.directory-data-relay-lambda.lambda_function_arn}
+      EOT
       }
-    }
+      
+      triggers = {
+          always_run = timestamp()
+      }
   }
 
 
