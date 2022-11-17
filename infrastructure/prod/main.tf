02d2ea00ba4b2ab27eeed1e1980cc76a63caa745
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2022-11-17 18:34:18 +0000

    Fix stage name

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -294,7 +294,7 @@ resource "aws_api_gateway_usage_plan" "standard" {
 
   api_stages {
     api_id = aws_api_gateway_rest_api.DoS_REST.id
-    stage  = aws_api_gateway_stage.main.main
+    stage  = aws_api_gateway_stage.main.stage_name
   }
 
   quota_settings {
