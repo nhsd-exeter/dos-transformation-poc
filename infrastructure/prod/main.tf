dd3f95cc62a5d9fcf1e7917496bfdc4253f3c470
Gregg Cowell <gdcowell@googlemail.com>
2023-01-18 13:33:13 +0000

    Decompose terraform main.tf

--- infrastructure/prod/main.tf
+++ infrastructure/prod/main.tf
@@ -8,7 +8,6 @@ data "aws_caller_identity" "current" {}
 provider "aws" {
   region = var.aws_region
 
-  skip_get_ec2_platforms      = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
   skip_credentials_validation = true
