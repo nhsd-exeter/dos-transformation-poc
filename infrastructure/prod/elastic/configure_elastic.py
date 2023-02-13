e41160a4964de8aa07fe6423be698cdb10aa021f
Gregg Cowell <64312449+gcowell@users.noreply.github.com>
2023-02-13 15:16:31 +0000

    Mapping debug

--- infrastructure/prod/elastic/configure_elastic.py
+++ infrastructure/prod/elastic/configure_elastic.py
@@ -14,8 +14,8 @@ index_names = ['directory-index', 'geo-profiles-index']
 
 def configure_elastic():
    
-    url = host + "/geo-profiles-index"
-    r = requests.delete(url, auth=awsauth, headers=headers)
+    # url = host + "/geo-profiles-index"
+    # r = requests.delete(url, auth=awsauth, headers=headers)
 
     for index in index_names:
         if check_index_exists(index) != True:
