a608569344efd95afcb52c1c720210b960c55403
Gregg Cowell <gdcowell@googlemail.com>
2023-03-13 19:04:24 +0000

    Test runner

--- microservices/directory-data-manager/deploy_service.sh
+++ microservices/directory-data-manager/deploy_service.sh
@@ -10,7 +10,7 @@ ENVIRONMENT_NAME=$2
 
 pip install -r requirements.txt --target .
 pip install chalice --target .
-chalice --help
+python3 chalice --help
 # package ./deployment
 cd ./deployment
 LAMBDA_OUTPUT=$(aws lambda update-function-code --function-name=$SERVICE_NAME --zip-file=fileb://deployment.zip --publish)
