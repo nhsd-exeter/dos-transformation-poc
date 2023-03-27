64738795d71f85d167a511ef6b77bf10288ac5b7
Gregg Cowell <gdcowell@googlemail.com>
2023-03-27 19:52:33 +0100

    First pass wireframe

--- microservices/admin-panel/src/components/ServiceManagement.js
+++ microservices/admin-panel/src/components/ServiceManagement.js
@@ -1,9 +1,30 @@
 import React, { Fragment, Component } from 'react';
 import { Navigate } from 'react-router-dom';
 import axios from 'axios';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 
 class ServiceManagement extends Component {
+  constructor () {
+    super()
+    this.state = { currentService: null }
+  }
+
+  componentDidMount() {
+    this.getService()
+      .then(result => this.setState({ currentService: result }) )
+  }
 
   getService = async () =>
   {
@@ -18,16 +39,17 @@ class ServiceManagement extends Component {
       }
     });
 
-    const {service} = await APIGateway.get();
-    
+    const {request} = await APIGateway.get();
+    const parsedResponse= await JSON.parse(request.response);
+    let service = await parsedResponse.body;
+
+    return service;
+
   }
 
   render(){
-    console.log(this.props)
 
-    this.getService();
-
-
+    if(!this.state.currentService){return null}
     return (
       <Fragment>
         {!this.props.auth.isAuthenticated && <Navigate to='/login' replace={true}/> }
@@ -35,10 +57,87 @@ class ServiceManagement extends Component {
           <p className="has-text-centered">
             <span className="tag is-primary">Service Management</span> Manage services</p>
         </div>
+<div><h2>{this.state.currentService.name}</h2></div>
+<div>
+
+<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <TableBody>
+            <TableRow>
+              <TableCell component="th" scope="row">
+                <p>Name</p>
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="right">
+                {this.state.currentService.name}
+              </TableCell>
+              <TableCell style={{ width: 160 }} align="right">
+                <p>Icon</p>
+              </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+</div>
+
+
+
+
+        <div>
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Location</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div>{this.state.currentService.name}</div>
+          </Typography>
+        </AccordionDetails>
+        </Accordion>
+        </div>
+        <div>
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Provider Organisation</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div>{this.state.currentService.name}</div>
+          </Typography>
+        </AccordionDetails>
+        </Accordion>
+        </div>
+        <div>
+        <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Coverage Area</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div>{this.state.currentService.name}</div>
+          </Typography>
+        </AccordionDetails>
+        </Accordion>
+        </div>
+
       </Fragment>
+
     )
   }
 }
 
 export default ServiceManagement
+
+
 
